093f4c9e6cc584f55d51b6c813b09af3d261e5c6
karpov.sv@gmail.com <karpov.sv@gmail.com>
2023-07-31 03:35:13 +0200

    Skypatrol does not work with multiprocessing, let's fall back to threading

--- run_celery.sh
+++ run_celery.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-watchmedo auto-restart -d lcserver -p '*.py' --ignore-patterns="*/.*" -- python3 -m celery -- -A lcserver worker --loglevel=info
+watchmedo auto-restart -d lcserver -p '*.py' --ignore-patterns="*/.*" -- python3 -m celery -- -A lcserver worker --loglevel=info -P threads
